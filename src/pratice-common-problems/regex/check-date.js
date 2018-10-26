d535562e3fcf8b33c73253438b362735a7e28798
Phan Duc Manh <manh.phanduc@vietis.vn>
2018-10-26 19:34:05 +0700

    update files

---
+++ src/pratice-common-problems/regex/check-date.js
@@ -0,0 +1,6 @@
+let str = "2003-11-20";
+let reg = /^((?:19|20)\d\d)[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/;
+
+let matches = str.match(reg);
+console.log(matches);
+
--- src/pratice-common-problems/regex/extract-data-string.js
+++ src/pratice-common-problems/regex/extract-data-string.js
@@ -1,8 +1,14 @@
-let reg = /(([A-Z]{1,}))\:([0-9]{1,})/gi;
 let strg = "some text is a form of map: (AB:1;CD:2;EF:3)";
 
-let matches = strg.match(reg);
+
 console.log(matches);
+
+function extractData(str) {
+    let reg = /(([A-Z]{1,}))\:([0-9]{1,})/gi;
+    let matches = str.match(reg);
+
+    
+}
 
 var str = "";
 for (var i = 0; i < matches.length; i++) {
