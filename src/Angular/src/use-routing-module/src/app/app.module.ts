a3c44f8f74724a4979cc3df88e8b75965b833db9
DucManhPhan <ducmanhphan93@gmail.com>
2019-03-17 07:55:17 +0700

    update files

--- src/Angular/src/use-routing-module/src/app/app.module.ts
+++ src/Angular/src/use-routing-module/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './core/dashboard/dashboard.component';
+import { SidebarComponent } from './core/sidebar/sidebar.component';
+import { BannerComponent } from './core/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SidebarComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
