c46ee0acbc45a02eca3ea84ab6ffcbdca6b9399a
vietlt95 <lethevietmta@gmail.com>
2019-08-28 15:29:58 +0700

    update files

---
+++ src/data-structures/linked-list/doubly-linked-list.js
@@ -0,0 +1,24 @@
+
+
+class Node {
+
+	constructor() {
+		this.next = null;
+		this.previous = null;
+		this.data = null;
+	}
+
+}
+
+class DoublyLinkedList {
+
+	constructor() {
+		this.head = null;
+		this.last = null;
+	}
+
+	insert(node) {
+		
+	}
+
+}
