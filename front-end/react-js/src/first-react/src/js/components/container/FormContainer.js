9dd8df8f3064fcc419fec2ef908a30167451a90d
ducmanhphan93@gmail.com <lethevietmta@gmail.com>
2019-09-20 11:07:09 +0700

    update files

--- front-end/react-js/src/first-react/src/js/components/container/FormContainer.js
+++ front-end/react-js/src/first-react/src/js/components/container/FormContainer.js
@@ -1,17 +1,24 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import Input from "../presentational/Input.jsx";
+import Input from "../presentational/Input.js";
+
+
+
 class FormContainer extends Component {
   constructor() {
     super();
+
     this.state = {
       seo_title: ""
     };
+
     this.handleChange = this.handleChange.bind(this);
   }
+
   handleChange(event) {
     this.setState({ [event.target.id]: event.target.value });
   }
+
   render() {
     const { seo_title } = this.state;
     return (
@@ -27,6 +34,7 @@ class FormContainer extends Component {
       </form>
     );
   }
+  
 }
 
 const wrapper = document.getElementById("create-article-form");
--- front-end/react-js/src/first-react/src/js/components/presentational/Input.js
+++ front-end/react-js/src/first-react/src/js/components/presentational/Input.js
@@ -1,5 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
+
+
+
 const Input = ({ label, text, type, id, value, handleChange }) => (
   <div className="form-group">
     <label htmlFor={label}>{text}</label>
@@ -13,6 +16,7 @@ const Input = ({ label, text, type, id, value, handleChange }) => (
     />
   </div>
 );
+
 Input.propTypes = {
   label: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
@@ -21,4 +25,5 @@ Input.propTypes = {
   value: PropTypes.string.isRequired,
   handleChange: PropTypes.func.isRequired
 };
+
 export default Input;
