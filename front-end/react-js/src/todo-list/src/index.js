f4dab2ebb4cdee637a53975fe9e2eaa351fe5640
ManhPD5 <ducmanhphan93@gmail.com>
2019-10-03 12:19:34 +0700

    update files

--- front-end/react-js/src/todo-list/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-import './index.css';
-import App from './App/App.js';
-
-render(<App />, document.getElementById('root'));
-
-
-module.hot.appcept();
